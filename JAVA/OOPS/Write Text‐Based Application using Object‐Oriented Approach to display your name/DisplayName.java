be44c49d86f4d42df53988dd9a856354d9340327
water1995 <genevievexal@hotmail.com>
2020-12-27 23:00:14 +0530

    27/12/2020

---
+++ JAVA/OOPS/Write Text‐Based Application using Object‐Oriented Approach to display your name/DisplayName.java
@@ -0,0 +1,21 @@
+/*Write Text Based Application using Object Oriented Approach to display your name*/
+class Name{
+
+  public void display(){
+
+    System.out.println("Genevieve Xalxo");
+  }
+} 
+
+public class DisplayName{
+
+  public static void main(String[] args){
+
+    Name n1 = new Name();
+    n1.display();
+
+
+  }
+
+  
+}
